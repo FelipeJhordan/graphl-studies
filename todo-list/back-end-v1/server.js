5f8ad68309cba8fe2d2d29a33bdd67bd1a6b1bdd
FelipeJhordan <felipejordan.alves@gmail.com>
2022-11-24 01:48:22 -0300

    feat: up old other studies code

--- todo-list/back-end-v1/server.js
+++ todo-list/back-end-v1/server.js
@@ -1,29 +1,31 @@
-import express from 'express'
-import cors from 'cors'
-import { graphqlHTTP } from 'express-graphql'
-import { graphlServer } from './presentation/resolver.js'
-import data from './data/tasks.json'
+import express from "express";
+import cors from "cors";
+import { graphqlHTTP } from "express-graphql";
+import { graphlServer } from "./presentation/resolver.js";
+import data from "./data/tasks.json" assert { type: "json" };
 
-
-const app = express()
-const port = 4000
+const app = express();
+const port = 4000;
 try {
-
-    app.use(cors())
-    app.use(express.json())
-    app.use(express.urlencoded({
-        extended: true
-    }))
-    
-    
-    app.use('/graphql', graphqlHTTP({
-        schema: graphlServer,
-        context: { tasks: data}
-    }))
-      
-    app.listen(port, () => {
-    console.log(`Running a server at http://localhost:${port}`)
+  app.use(cors());
+  app.use(express.json());
+  app.use(
+    express.urlencoded({
+      extended: true,
     })
-} catch(e ) {
-    console.log(e)
+  );
+
+  app.use(
+    "/graphql",
+    graphqlHTTP({
+      schema: graphlServer,
+      context: { tasks: data },
+    })
+  );
+
+  app.listen(port, () => {
+    console.log(`Running a server at http://localhost:${port}`);
+  });
+} catch (e) {
+  console.log(e);
 }
